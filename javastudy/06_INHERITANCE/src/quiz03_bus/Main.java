c9a4ddae32d9a2e400dd9bf1bf6f4b1129aa93a6
sera <sera7057@naver.com>
2022-08-01 16:10:26 +0900

    2022-08-01

---
+++ javastudy/06_INHERITANCE/src/quiz03_bus/Main.java
@@ -0,0 +1,16 @@
+package quiz03_bus;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Bus bus = new Bus(25);
+		bus.ride(1, new Person("kim"));
+		bus.ride(5, new Student("choi"));
+		bus.ride(10, new Alba("min"));
+		bus.info();
+	
+
+	}
+
+}
