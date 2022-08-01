a5813a3e14b45b435fd98f31505622784c5d4171
sera <sera7057@naver.com>
2022-08-01 10:53:43 +0900

    2022-07-27

---
+++ javastudy/06_INHERITANCE/src/ex05_constructor/StudentMain.java
@@ -0,0 +1,12 @@
+package ex05_constructor;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student(); // 서브클래스 생성자 호출, 슈퍼클래스의 생성자도 같이 호출됨.
+
+	}
+
+}
+
