8feab06c06588c074b04b9cc10631a6d99bc82bd
sera <sera7057@naver.com>
2022-08-01 12:50:17 +0900

    2022-08-01

--- javastudy/06_INHERITANCE/src/ex07_override/Americano.java
+++ javastudy/06_INHERITANCE/src/ex07_override/Americano.java
@@ -7,6 +7,7 @@ public class Americano extends Espresso {
 	@Override
 	public void taste() {
 		System.out.println("덜 쓰다");
+
 	}
 
 }
---
+++ javastudy/06_INHERITANCE/src/quiz02_coffee/Coffee.java
@@ -0,0 +1,25 @@
+package quiz02_coffee;
+
+public class Coffee {
+
+	private String origin;
+	
+	
+
+	public Coffee(String origin) {
+		super();
+		this.origin = origin;
+	}
+
+
+
+	public void info() {
+		System.out.println(origin + "원두");
+	}
+
+	
+	
+	
+	
+	
+}
