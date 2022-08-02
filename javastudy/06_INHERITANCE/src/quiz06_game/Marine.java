298c8e9e363c47314ffacabb33d7eb7f08e4002f
sera <sera7057@naver.com>
2022-08-02 17:21:47 +0900

    2022-08-02

---
+++ javastudy/06_INHERITANCE/src/quiz06_game/Marine.java
@@ -0,0 +1,26 @@
+package quiz06_game;
+
+public class Marine extends GameUnit {
+	
+	public Marine(String name) {
+		super(name, 50,5);
+	}
+	
+	@Override
+	public void attack(GameUnit unit) {
+		// 40% 확률로 KO 시킬 수 있다.
+		if(Math.random() < 0.4) {
+			unit.setEnergy(0);
+			unit.setAlive(false);
+			System.out.println(unit.getName() + "을 한 방에 죽였다.");
+		} else {
+			int unitEnergy = unit.getEnergy() - getPower() < 0 ? 0 : unit.getEnergy() - getPower();
+			unit.setEnergy(unitEnergy);
+			unit.setAlive(unitEnergy > 0);
+			System.out.println(unit.getName() + "의 남은 에너지 " + unit.getEnergy());
+		}
+	}
+		
+
+	
+}
