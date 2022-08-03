bad8a900ddaa23e988f8f67bbf80fce075907b93
sera <sera7057@naver.com>
2022-08-03 12:06:03 +0900

    2022-08-03

---
+++ javastudy/06_INHERITANCE/src/quiz07_song/Main.java
@@ -0,0 +1,20 @@
+package quiz07_song;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Singer singer = new Singer("선미", 2);
+		
+		Song song1 = new Song("노래1", 3.5);
+		Song song2 = new Song("노래2", 4.5);
+		
+		Producer producer = new Producer();
+		producer.produce(singer, song1);
+		producer.produce(singer, song2);
+		
+		singer.info();
+
+	}
+
+}
---
+++ javastudy/06_INHERITANCE/src/quiz07_song/Producer.java
@@ -0,0 +1,12 @@
+package quiz07_song;
+
+public class Producer {
+	
+	
+	public void produce(Singer singer, Song song) {
+		singer.addSong(song);
+	}
+	
+	
+
+}
