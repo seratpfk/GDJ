2f27b9da71b25bb68a169fb725b74d74c54eaa7b
sera <sera7057@naver.com>
2022-08-11 17:46:32 +0900

    2022-08-11

--- javastudy/10_IO/src/ex03_reader/XMLReader.java
+++ javastudy/10_IO/src/ex03_reader/XMLReader.java
@@ -1,6 +1,8 @@
 package ex03_reader;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -25,20 +27,34 @@ public class XMLReader {
 			Element root = document.getDocumentElement();
 			System.out.println("최상위 요소 : " + root.getNodeName());
 			
+			List<Product> products = new ArrayList<Product>();
 			// 최상위 요소의 자식 노드들
 			NodeList nodeList = root.getChildNodes();
 			for(int i = 0; i < nodeList.getLength(); i++) {
 				Node node = nodeList.item(i);  // 줄 바꾼(#text)과 <product>태그로 구성
 				if(node.getNodeType() == Node.ELEMENT_NODE) { // 노드가 Element인가?(줄바꿈 #text 제외되고 <product>태그만 남음) 
 					NodeList nodeList2 = node.getChildNodes(); // <product> 태그의 자식노드(줄바꿈 #text, <number>, <name>, <price> 태그)
+					Product product = new Product();
 					for(int j = 0; j < nodeList2.getLength(); j++) {
 						Node node2 = nodeList2.item(j);
 						if(node2.getNodeType() == Node.ELEMENT_NODE) {
-							System.out.println(node2.getNodeName() + node2.getTextContent());
+							switch(node2.getNodeName()) {
+							case "number": product.setNumber(node2.getTextContent()); break;
+							case "name": product.setName(node2.getTextContent()); break;
+							case "price": product.setPrice(Integer.parseInt(node2.getTextContent())); break;
+							
+							}
 						}
 					}
+					// ArrayList에 product 추가
+					products.add(product);
 				}
 				
+			}
+			
+			// ArrayList 확인
+			for(Product product : products) {
+				System.out.println(product);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
