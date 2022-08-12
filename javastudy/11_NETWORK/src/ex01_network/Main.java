370dba9fbbebeff23289b18b1a26fb28e550b91e
sera <sera7057@naver.com>
2022-08-12 17:12:07 +0900

    2022-08-12

--- javastudy/11_NETWORK/src/ex01_network/Main.java
+++ javastudy/11_NETWORK/src/ex01_network/Main.java
@@ -1,5 +1,11 @@
 package ex01_network;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -34,10 +40,91 @@ public class Main {
 			System.out.println("API 주소 오류");
 		}
 	}
+	
+	public static void m2() {
+		
+		// HttpURLConnection 클래스
+		// 1. 웹 접속을 담당하는 클래스
+		// 2. URL 클래스와 함께 사용
+		// 3. URLConnection 클래스의 자식 클래스
+		// 4. URL 클래스의 openConnection() 메소드를 호출해서 HttpURLConnection 클래스 타입으로 저장
+		
+		try {
+			
+			String apiURL = "https://www.google.com";  // 구글 접속
+			URL url = new URL(apiURL);
+			
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			
+			// HTTP 응답 코드
+			// 1. 200 : 정상
+			// 2. 40X : 요청이 잘못됨(사용자 잘못)
+			// 3. 50X : 서버 오류
+			
+			System.out.println("응답 코드 : " + con.getResponseCode());
+			System.out.println("정상 : " + HttpURLConnection.HTTP_OK);
+			System.out.println("Not Found : " + HttpURLConnection.HTTP_NOT_FOUND);
+			System.out.println("Internal Error : " + HttpURLConnection.HTTP_INTERNAL_ERROR);
+			System.out.println("컨텐트 타입 : " + con.getContentType());
+			System.out.println("요청 방식 : " + con.getRequestMethod());
+			
+			con.disconnect(); // 접속 해제(생략 가능)
+			
+			
+		} catch(MalformedURLException e) {  // URL 클래스
+			System.out.println("API 주소 오류");
+		} catch(IOException e) {  // HttpURLConnection 클래스
+			System.out.println("API 접속 오류");
+		}
+	}
+	
+	
+	public static void m3() {
+		
+		// HttpURLConnection과 스트림
+		
+		try {
+			
+			String apiURL = "https://www.naver.com";
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			
+			// 바이트 입력 스트림
+			InputStream in = con.getInputStream();
+			
+			// 문자 입력 스트림으로 변환
+			InputStreamReader reader = new InputStreamReader(in);
+			
+			// 모두 읽어서 StringBuilder에 저장
+			StringBuilder sb = new StringBuilder();
+			char[] cbuf = new char[100]; // 100글자씩 처리
+			int readCnt = 0; // 실제로 읽은 글자 수
+			
+			while((readCnt = reader.read(cbuf)) != -1) {
+				sb.append(cbuf, 0, readCnt);
+			}
+			
+			// StringBuilder의 모든 내용을 C:\\storage\\naver.html로 내보내기
+			
+			File file = new File("C:\\storage", "naver.html");
+			FileWriter fw = new FileWriter(file);
+			
+			fw.write(sb.toString());
+			
+			fw.close();
+			reader.close();
+			con.disconnect();
+			
+		} catch(MalformedURLException e) {
+			System.out.println("API 주소 오류");
+		} catch(IOException e) {
+			System.out.println("API 접속 및 연결 오류");
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		m1();
+		m3();
 
 	}
 
