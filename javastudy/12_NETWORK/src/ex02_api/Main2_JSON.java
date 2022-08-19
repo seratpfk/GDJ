ab8929abc90138a87f6a04e9661f2a06d7db68af
sera <sera7057@naver.com>
2022-08-19 12:51:13 +0900

    2022-08-19

--- javastudy/12_NETWORK/src/ex02_api/Main2_JSON.java
+++ javastudy/12_NETWORK/src/ex02_api/Main2_JSON.java
@@ -226,13 +226,106 @@ public class Main2_JSON {
 		}
 		
 	}
+	
+	
+public static String m5() {
+		
+		// 소상공인시장진흥공단_상가(상권)정보
+		
+		// 인증키(Decoding)
+		String serviceKey = 	
+				"0+KGdm6YrHSXA95CGIey5Sk0Sahzg8aiIbzdSRfUSCSABUGREXTJKtUr3IO+48dPKEQEZudz2n78Q/tx5pHIBQ==";
+
+		// API 주소	(주소 + 요청 파라미터)
+		StringBuilder urlBuilder = new StringBuilder();
+		try {
+			urlBuilder.append("http://apis.data.go.kr/B553077/api/open/sdsc2/storeZoneOne");
+			urlBuilder.append("?serviceKey=").append(URLEncoder.encode(serviceKey, "UTF-8"));
+			urlBuilder.append("&key=9940");
+			urlBuilder.append("&type=json");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		String apiURL = urlBuilder.toString();
+		
+		// API 주소 접속
+		URL url = null;
+		HttpURLConnection con = null;
+		
+		try {			
+			url = new URL(apiURL);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/JSON; charset=UTF-8");
+		} catch(MalformedURLException e) {
+			System.out.println("API 주소 오류");
+		} catch(IOException e) {
+			System.out.println("API 접속 실패");
+		}
+		
+		// 입력 스트림 생성
+		// 1. 서버가 보낸 데이터를 읽어야 하므로 입력 스트림이 필요
+		// 2. 서버와 연결된 입력 스트림은 바이트 스트림이므로 문자 스트림으로 변환해야 함
+		BufferedReader reader = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			
+			if(con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			
+			// 스트림 종료
+			reader.close();
+			
+		} catch(IOException e) {
+			System.out.println("API 응답 실패");
+		}
+		
+		// API로부터 전달받은 json 데이터
+		String response = sb.toString();
+		
+		// json File 생성
+		File file = new File("C:\\storage", "api3.json");
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+			bw.write(response);
+			bw.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return response;
+		
+	}
+
+
+
+public static void m6() {
+	
+	JSONObject obj = new JSONObject(m5());
+	JSONObject header = obj.getJSONObject("header");
+	JSONArray columns = header.getJSONArray("columns");
+	for(int i = 0; i < columns.length(); i++) {
+		System.out.println(columns.getString(i));
+	}
+}
 
 	public static void main(String[] args) {
 		
 		//m1();
 		//m2();
-		m3();
-		m4();
+		//m3();
+		//m4();
+		m5();
+		m6();
 
 	}
 
