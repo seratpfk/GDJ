257f738c104e5545cb9db6b79a928bdb9568829d
sera <sera7057@naver.com>
2022-08-25 12:48:07 +0900

    2022-08-25

--- dbstudy/08_DML(부서,사원).sql
+++ dbstudy/08_DML(부서,사원).sql
@@ -24,3 +24,57 @@ ALTER TABLE EMPLOYEE
     ADD CONSTRAINT FK_EMPLOYEE_DEPARTMENT FOREIGN KEY(DEPART)
         REFERENCES DEPARTMENT(DEPT_NO)
             ON DELETE SET NULL;
+            
+/**********************************************************************/
+
+/*
+    DML
+    1. Data Manipulation Language
+    2. 데이터 조작어
+    3. 행(Row, Record, Tuple) 단위 삽입, 수정, 삭제
+    4. 트랜잭션(작업) 완료를 위해 COMMIT이 필요
+    5. 트랜잭션(작업) 취소를 위해 ROLLBACK 사용 가능
+    6. 종류
+        1) INSERT INTO VALUES
+        2) UPDATE SET WHERE
+        3) DELETE FROM WHERE
+*/
+
+/*
+-- 행 삽입
+-- 1. 지정한 칼럼에 삽입
+-- INSERT INTO 테이블(칼럼1, 칼럼2, ...) VALUES(값1, 값2, ...)
+-- 2. 모든 칼럼에 데이터 삽입
+-- INSERT INTO 테이블 VALUES(값1, 값2, ...)
+*/
+
+-- 부서 테이블에 행(Row) 삽입
+-- 부모 테이블(관계에서 PK를 가진 테이블)에 먼저 삽입을 해야 함
+INSERT INTO DEPARTMENT
+    (DEPT_NO, DEPT_NAME, LOCATION)
+VALUES
+    (1, '영업부', '대구');
+INSERT INTO DEPARTMENT
+    (DEPT_NO, DEPT_NAME, LOCATION)
+VALUES
+    (2, '인사부', '서울');
+INSERT INTO DEPARTMENT
+    (DEPT_NO, DEPT_NAME, LOCATION)
+VALUES
+    (3, '총무부', '대구');
+INSERT INTO DEPARTMENT
+    (DEPT_NO, DEPT_NAME, LOCATION)
+VALUES
+    (4, '기획부', '서울');
+
+-- 작업의 완료
+COMMIT;
+
+INSERT INTO DEPARTMENT(DEPT_NO, DEPT_NAME, LOCATION) VALUES(5, '개발부', '인천');
+
+
+
+    
+
+
+        
