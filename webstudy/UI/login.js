44f18a5c564b77f73642a004c6e417dcd17f93aa
sera <sera7057@naver.com>
2022-10-06 19:49:36 +0900

    2022-10-06

---
+++ webstudy/UI/login.js
@@ -0,0 +1,23 @@
+ /* 비밀번호 입력 없이 Sign In 버튼 누르면
+경고창 띄우기 (비밀번호를 입력하세요.) */
+
+document.getElementById('btn_signin').onclick = function(event) { /* signinin버튼 id */
+    var pw = document.getElementById('pw');
+    if(pw.value == '') {
+        alert('비밀번호를 입력하세요');
+        event.preventDefault(); /* 서브밋 막기 */
+        return;
+    }
+}
+document.getElementById('id').onkeyup = function(event){
+    var id = document.getElementById('id');
+    var id_msg = document.getElementById('id_msg');
+    if(id.value.length == 0){
+        id_msg.textContent = ''; /* 길이가 0이면 text 없음 */
+    } else if(id.value.length < 4) {
+        id_msg.textContent = '아이디는 4자 이상입니다.';
+    } else if(id.value.length >= 4) {
+        id_msg.textContent = '정상적인 아이디입니다.';
+    }
+}
+
