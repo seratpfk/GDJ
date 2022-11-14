1e75e4c21595e1f9287f29d09219a0fc1a58abf0
sera <sera7057@naver.com>
2022-11-14 17:30:30 +0900

    2022-11-14

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserServiceImpl.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/service/UserServiceImpl.java
@@ -12,18 +12,29 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import com.gdu.app13.mapper.UserMapper;
 import com.gdu.app13.util.SecurityUtil;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
+@PropertySource(value = {"classpath:email.properties"})
 @Service
 public class UserServiceImpl implements UserService {
 
+	// 이메일을 보내는 사용자 정보
+	@Value(value = "${mail.username}")
+	private String username;  // 본인 지메일 주소
+	
+	@Value(value="${mail.password}")
+	private String password;  // 발급 받은 앱 비밀번호
+	
+	@Autowired
 	private UserMapper userMapper;
+	
+	@Autowired
 	private SecurityUtil securityUtil;
 	
 	@Override
@@ -66,11 +77,6 @@ public class UserServiceImpl implements UserService {
 			        (2) 기기 선택 : Windows 컴퓨터
 			        (3) 생성 버튼 : 16자리 앱 비밀번호를 생성해 줌(이 비밀번호를 이메일 보낼 때 사용)
 		*/
-		
-		
-		// 이메일을 보내는 사용자 정보
-		String username = "serasr0828@gmail.com";  // 본인 지메일 주소
-		String password = "txcparqzzwowlqnn";        // 발급 받은 앱 비밀번호
 		
 		// 사용자 정보를 javax.mail.Session에 저장
 		Session session = Session.getInstance(properties, new Authenticator() {
